59c7041735206c46c9e3a30ceb7e5fc6179bd9b6
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-23 01:53:24 +0200

    header: added color on hover and class active, (font size, weight and color to the navbar) and size to img logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,44 +13,71 @@ export default {
 <template>
 
     <header>
-        <!-- IMG LOGO DC COMICS -->
-        <div class="logo-wrapper">
-            <img src="../assets/img/dc-logo.png" alt="">
-        </div>
+        <div class="container">
 
-        <!-- NAVBAR  -->
-        <div class="navbar">
-            <nav>
-                <ul>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">COMICS</a></li>
-                    <li><a href="#">MOVIES</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">GAMES</a></li>
-                    <li><a href="#">COLLECTIBLES</a></li>
-                    <li><a href="#">VIDEOS</a></li>
-                    <li><a href="#">FANS</a></li>
-                    <li><a href="#">NEWS</a></li>
-                    <li><a href="#">SHOP</a></li>
-                </ul>
-            </nav>
-        </div>
+            <!-- IMG LOGO DC COMICS -->
+            <div class="logo-wrapper">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
 
+            <!-- NAVBAR  -->
+            <div class="navbar">
+                <nav>
+                    <ul>
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
     </header>
 </template>
 
 <style scoped>
 header {
     background: #fff;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
 
-    ul {
+    .container {
+
         display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 150px;
 
-        li {
-            margin: 0 10px;
+        .logo-wrapper {
+            width: 80px;
+
+            img {
+                width: 100%;
+            }
+        }
+
+        ul {
+            display: flex;
+
+            li {
+                margin: 0 10px;
+
+                a {
+                    font: 800 12px Arial;
+                    color: #636363;
+
+                    &:hover,
+                    &.active {
+                        color: #0182F9;
+                    }
+
+                }
+            }
         }
     }
 
