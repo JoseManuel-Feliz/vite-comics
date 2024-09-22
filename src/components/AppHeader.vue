d51dfbe0532ae0cc1e2b161215d8f81ae5b8ddf2
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-22 20:55:49 +0200

    added: header bg-color, flexbox properties, side margin to list items

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,43 +1,58 @@
 <script>
-export default{
-    data(){
-        return{
+export default {
+    data() {
+        return {
 
-            }
         }
-
     }
+
+}
 
 </script>
 
 <template>
 
-<header>
-<!-- IMG LOGO DC COMICS -->
-   <div class="logo-wrapper">
-<img src="../assets/img/dc-logo.png" alt="">
-   </div>
-   
-<!-- NAVBAR  -->
-<div class="navbar">
-<nav>
-    <ul>
-        <li>CHARACTERS</li>
-        <li>COMICS</li>
-        <li>MOVIES</li>
-        <li>TV</li>
-        <li>GAMES</li>
-        <li>COLLECTIBLES</li>
-        <li>VIDEOS</li>
-        <li>FANS</li>
-        <li>NEWS</li>
-        <li>SHOP</li>
-    </ul>
-</nav>
-</div>
+    <header>
+        <!-- IMG LOGO DC COMICS -->
+        <div class="logo-wrapper">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
 
-</header>
+        <!-- NAVBAR  -->
+        <div class="navbar">
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
 </template>
 
 <style scoped>
+header {
+    background: #fff;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    ul {
+        display: flex;
+
+        li {
+            margin: 0 10px;
+        }
+    }
+
+}
 </style>
