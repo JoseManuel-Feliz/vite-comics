2cb080e135491205f2a44e8365ad063bfcca0d7c
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-09-22 21:02:10 +0200

    reset all 'a' tags, all 'ul', added bg-c to the body

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,26 @@ import AppHeader from "./components/AppHeader.vue"
 import AppMain from "./components/AppMain.vue"
 import AppFooter from "./components/AppFooter.vue"
 
-export default{
-  data(){
-    return{
+export default {
+  data() {
+    return {
 
     }
-    
+
   },
-  components:{
-  AppHeader,
-  AppMain,
-  AppFooter  
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
   }
 }
 
 </script>
 
 <template>
-<AppHeader/>
-<AppMain/>
-<AppFooter/>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 <style lang="scss">
