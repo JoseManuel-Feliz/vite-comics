699597a362a2528a093f05c735a1fe6ad0bbc283
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-09-22 17:45:59 +0200

    added footer top navbar links and footer bottom navbar (sign-up btn and social media links)

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <script>
-export default{
-    data(){
+export default {
+    data() {
         return {
 
         }
@@ -10,13 +10,69 @@ export default{
 </script>
 
 <template>
-<footer>
-    <h1>
-        hellossadje
-</h1>   
-</footer>
+    <footer>
+
+        <div class="">
+            <nav class="footer-navbar">
+
+                <ul>
+                    <li></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+
+                <ul>
+                    <li></li>
+                    <li><a href="#">Shop DC </a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+
+                <ul>
+                    <li></li>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy police (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+
+                <ul>
+                    <li></li>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="">
+            <a href="#" class="btn">SIGN-UP NOW!</a>
+
+            <div>
+                <ul>
+                    <li></li>
+                    <li><a href="#"><img src="../assets/img/footer-facebook.png" alt=""></a></li>
+                    <li><a href="#"><img src="../assets/img/footer-twiter" alt=""></a></li>
+                    <li><a href="#"><img src="../assets/img/footer-youtube.png" alt=""></a></li>
+                    <li><a href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a></li>
+                    <li><a href="#"><img src="../assets/img/footer-periscope.png" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
