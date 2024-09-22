90baeaded4da89dc96ca561d5be496dd58a6817a
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-22 16:44:43 +0200

    added components: AppHeader, AppMain, AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+    data(){
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<template>
+<footer>
+    <h1>
+        hellossadje
+</h1>   
+</footer>
+</template>
+
+<style scoped>
+
+</style>
