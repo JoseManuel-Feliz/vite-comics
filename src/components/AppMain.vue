009a317e7ff8d9c0b4df64ab4a5b5a22b7cb2d19
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-23 01:12:55 +0200

    added: flex-porperties (in the main, main-navbar, main sections), bg-c in the main navbar, added sizes (to the content-wrapper, main-navbar)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,27 +11,103 @@ export default {
 
 <template>
     <main>
-        <div class="content-wrapper">
-            <p>--> content goes here <-- </p>
-        </div>
 
-        <div class="main-navbar">
+        <section>
+            <div class="content-wrapper">
+                <p class="main-content">--> content goes here <-- </p>
+            </div>
+        </section>
 
-            <nav>
+        <section class="wrapper main-navbar">
+            <nav class="main-navbar">
                 <ul>
-                    <li><a href="#">DIGITAL COMICS</a></li>
-                    <li><a href="#">DC MERCHANDISE</a></li>
-                    <li><a href="#">SUBCRUPTION</a></li>
-                    <li><a href="#">COMIC SHOP LOCATOR</a></li>
-                    <li><a href="#">DC POWER VISA</a></li>
+
+                    <li><a href="#"><img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                            <p>DIGITAL COMICS</p>
+                        </a>
+                    </li>
+                    <li><a href="#"><img src="../assets/img/buy-comics-merchandise.png" alt="">
+                            <p>DC MERCHANDISE</p>
+                        </a></li>
+                    <li><a href="#"><img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                            <p>SUBCRUPTION</p>
+                        </a></li>
+                    <li><a href="#"><img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                            <p>COMIC SHOP LOCATOR</p>
+                        </a>
+                    </li>
+                    <li><a href="#"><img class="w-60" src="../assets/img/buy-dc-power-visa.svg" alt="">
+                            <p>DC POWER VISA</p>
+                        </a></li>
                 </ul>
             </nav>
-        </div>
+        </section>
     </main>
 </template>
 
 <style scoped>
-.main-navbar {
-    background: #0182F9;
+main {
+
+    display: flex;
+    flex-direction: column;
+
+    section {
+        flex-grow: 1;
+
+
+
+        .content-wrapper {
+            margin: 0 auto;
+            max-width: 1200px;
+            height: 150px;
+            display: flex;
+            align-items: center;
+
+            p {
+                font-size: 2rem;
+                color: #fff;
+            }
+        }
+    }
+
+    .wrapper.main-navbar {
+        background: #0182F9;
+
+        .main-navbar {
+            max-width: 1200px;
+            margin: 0 auto;
+
+
+            ul {
+                display: flex;
+                height: 150px;
+
+
+
+                li {
+                    flex-basis: calc(100% / 5);
+                    display: flex;
+                    justify-content: space-around;
+                    align-items: center;
+
+                    a {
+                        display: flex;
+                        height: 50px;
+
+                        p {
+                            align-self: center;
+                            color: #fff;
+                            margin-left: 10px;
+                            font-size: 18px;
+                        }
+                    }
+                }
+            }
+        }
+
+        .w-60 {
+            width: 60px;
+        }
+    }
 }
 </style>
