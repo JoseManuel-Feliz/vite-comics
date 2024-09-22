552f2a2b38e925cabfd579827a2dc382e78be195
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-09-22 17:15:40 +0200

    added to AppHeader img logo and navbar menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,10 +11,31 @@ export default{
 </script>
 
 <template>
+
 <header>
-    <h1>
-        hello   
-    </h1>
+<!-- IMG LOGO DC COMICS -->
+   <div class="logo-wrapper">
+<img src="../assets/img/dc-logo.png" alt="">
+   </div>
+   
+<!-- NAVBAR  -->
+<div class="navbar">
+<nav>
+    <ul>
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+    </ul>
+</nav>
+</div>
+
 </header>
 </template>
 
