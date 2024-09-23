75e02ca95487e40e2825c3aba78b43ccdff1da51
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-23 05:18:45 +0200

    added socials links navbar

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,60 +13,66 @@ export default {
     <footer>
         <div class="bg-wrapper">
 
-            <div class="wrapper">
-                <nav class="footer-navbar">
+            <div class="container">
 
-                    <ul>
-                        <li></li>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">News</a></li>
-                    </ul>
+                <div class="wrapper">
+                    <nav class="footer-navbar">
 
-                    <ul>
-                        <li></li>
-                        <li><a href="#">Shop DC </a></li>
-                        <li><a href="#">Shop DC Collectibles</a></li>
-                    </ul>
+                        <ul>
+                            <li></li>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
 
-                    <ul>
-                        <li></li>
-                        <li><a href="#">Terms Of Use</a></li>
-                        <li><a href="#">Privacy police (New)</a></li>
-                        <li><a href="#">Ad Choices</a></li>
-                        <li><a href="#">Advertising</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Subscriptions</a></li>
-                        <li><a href="#">Talent Workshops</a></li>
-                        <li><a href="#">CPSC Certificates</a></li>
-                        <li><a href="#">Ratings</a></li>
-                        <li><a href="#">Shop Help</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
+                        <ul>
+                            <li></li>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
 
-                    <ul>
-                        <li></li>
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
-                    </ul>
-                </nav>
+                        <ul>
+                            <li></li>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy police (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+
+                        <ul class="flex-grow">
+                            <li></li>
+                            <li><a href="#">Shop DC </a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
+        </div>
 
-            <div class="">
+        <div class="bg-grey">
+
+            <div class="container barra">
                 <a href="#" class="btn">SIGN-UP NOW!</a>
 
-                <div>
+                <div class="socials-links">
                     <ul>
                         <li></li>
                         <li><a href="#"><img src="../assets/img/footer-facebook.png" alt=""></a></li>
-                        <li><a href="#"><img src="../assets/img/footer-twiter" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/img/footer-twitter.png" alt=""></a></li>
                         <li><a href="#"><img src="../assets/img/footer-youtube.png" alt=""></a></li>
                         <li><a href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a></li>
                         <li><a href="#"><img src="../assets/img/footer-periscope.png" alt=""></a></li>
@@ -81,33 +87,71 @@ export default {
 footer {
 
     .bg-wrapper {
+        background: url('../assets/img/footer-bg.jpg') 0 0 / cover no-repeat;
+        height: 350px;
 
-        background-image: url('../assets/img/dc-logo-bg.png'), url('../assets/img/footer-bg.jpg');
-        background-repeat: no-repeat, no-repeat;
-        background-size: 30%, cover;
-        background-position: right center, 0 0;
+        .container {
+            height: 100%;
+            max-width: 1200px;
+            margin: 0 auto;
+            background-image: url('../assets/img/dc-logo-bg.png');
+            background-repeat: no-repeat;
+            background-position: right;
 
-        .wrapper {
-            display: flex;
-            justify-content: space-around;
-
-            .footer-navbar {
+            .wrapper {
                 display: flex;
+                padding: 40px 0;
 
-                ul {
+                .footer-navbar {
                     display: flex;
-                    flex-direction: column;
+                    column-gap: 15px;
+                    flex-wrap: wrap;
 
-                    li {
+
+                    ul {
+                        display: flex;
+                        flex-direction: column;
+                        flex-basis: calc(100% / 3 - 45px / 3);
+
+
 
                         a {
+                            font-size: small;
                             color: lightgray;
                         }
+
                     }
                 }
+
             }
 
-            .footer-logo {}
+        }
+    }
+
+    .bg-grey {
+        background-color: #303030;
+
+        .barra {
+            height: 100px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            a.btn {
+                font-size: 18px;
+                color: #fff;
+                padding: 15px;
+                border: #0182F9 solid 3px;
+            }
+
+            .socials-links>ul {
+                display: flex;
+
+                li {
+                    margin: 0 10px;
+                }
+
+            }
         }
     }
 }
