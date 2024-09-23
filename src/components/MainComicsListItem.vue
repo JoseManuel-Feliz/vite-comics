fbf7030420d93dc8f16c56ad54cbbb2adbb51a0f
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-09-23 18:13:26 +0200

    added prop property in list MainListIems

---
+++ src/components/MainComicsListItem.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        comicsUrl: {
+            required: true,
+            type: String
+        },
+        comicSeries: {
+            required: true,
+            type: String
+        },
+        comicPrice: {
+            required: true,
+            type: String
+        },
+        comicType: {
+            required: true,
+            type: String
+        },
+    },
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped></style>
