8b2d8e402dcf4f4ab2fb3c57f822d383a576ef70
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-23 02:39:09 +0200

    footer: added (bg, logo bg to footer), flexbox properties, to footer navbar

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,75 +11,104 @@ export default {
 
 <template>
     <footer>
+        <div class="bg-wrapper">
 
-        <div class="">
-            <nav class="footer-navbar">
+            <div class="wrapper">
+                <nav class="footer-navbar">
 
-                <ul>
-                    <li></li>
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">News</a></li>
-                </ul>
+                    <ul>
+                        <li></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
 
-                <ul>
-                    <li></li>
-                    <li><a href="#">Shop DC </a></li>
-                    <li><a href="#">Shop DC Collectibles</a></li>
-                </ul>
+                    <ul>
+                        <li></li>
+                        <li><a href="#">Shop DC </a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
 
-                <ul>
-                    <li></li>
-                    <li><a href="#">Terms Of Use</a></li>
-                    <li><a href="#">Privacy police (New)</a></li>
-                    <li><a href="#">Ad Choices</a></li>
-                    <li><a href="#">Advertising</a></li>
-                    <li><a href="#">Jobs</a></li>
-                    <li><a href="#">Subscriptions</a></li>
-                    <li><a href="#">Talent Workshops</a></li>
-                    <li><a href="#">CPSC Certificates</a></li>
-                    <li><a href="#">Ratings</a></li>
-                    <li><a href="#">Shop Help</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
+                    <ul>
+                        <li></li>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy police (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
 
-                <ul>
-                    <li></li>
-                    <li><a href="#">DC</a></li>
-                    <li><a href="#">MAD Magazine</a></li>
-                    <li><a href="#">DC Kids</a></li>
-                    <li><a href="#">DC Universe</a></li>
-                    <li><a href="#">DC Power Visa</a></li>
-                </ul>
-            </nav>
-        </div>
-
-        <div class="">
-            <a href="#" class="btn">SIGN-UP NOW!</a>
-
-            <div>
-                <ul>
-                    <li></li>
-                    <li><a href="#"><img src="../assets/img/footer-facebook.png" alt=""></a></li>
-                    <li><a href="#"><img src="../assets/img/footer-twiter" alt=""></a></li>
-                    <li><a href="#"><img src="../assets/img/footer-youtube.png" alt=""></a></li>
-                    <li><a href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a></li>
-                    <li><a href="#"><img src="../assets/img/footer-periscope.png" alt=""></a></li>
-                </ul>
+                    <ul>
+                        <li></li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </nav>
             </div>
-        </div>
-        <div>
-            <img src="../assets/img/dc-logo-bg.png" alt="">
+
+            <div class="">
+                <a href="#" class="btn">SIGN-UP NOW!</a>
+
+                <div>
+                    <ul>
+                        <li></li>
+                        <li><a href="#"><img src="../assets/img/footer-facebook.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/img/footer-twiter" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/img/footer-youtube.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/img/footer-periscope.png" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
 
 <style scoped>
 footer {
-    background: url('../assets/img/footer-bg.jpg') 0 0 / contain no-repeat;
+
+    .bg-wrapper {
+
+        background-image: url('../assets/img/dc-logo-bg.png'), url('../assets/img/footer-bg.jpg');
+        background-repeat: no-repeat, no-repeat;
+        background-size: 30%, cover;
+        background-position: right center, 0 0;
+
+        .wrapper {
+            display: flex;
+            justify-content: space-around;
+
+            .footer-navbar {
+                display: flex;
+
+                ul {
+                    display: flex;
+                    flex-direction: column;
+
+                    li {
+
+                        a {
+                            color: lightgray;
+                        }
+                    }
+                }
+            }
+
+            .footer-logo {}
+        }
+    }
 }
 </style>
